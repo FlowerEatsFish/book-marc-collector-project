758353ba72ecc5e8f00c700e8a8f1fc64aeda4f0
Eating-Fish Flower <flower.eating.fish@gmail.com>
2018-03-30 14:38:02 +0800

    Initial workshop

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  watch: true,
+  entry: {
+    bundle: './src/master.jsx',
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: 'dist/',
+    filename: './js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+        }, {
+          loader: 'css-loader',
+        }],
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+};
