5a877cec91f126986b8fdb3b42e4e923d3efa7c7
Eating-Fish Flower <flower.eating.fish@gmail.com>
2018-03-31 14:09:21 +0800

    Ver0.1.0 Initial components

---
+++ src/master.jsx
@@ -0,0 +1,10 @@
+/* global document */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/app.jsx';
+import './style/global.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
